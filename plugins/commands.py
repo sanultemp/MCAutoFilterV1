f1b3c24ae6724742ebcabb594e68a2b661defae8
sanultemp <88481114+sanultemp@users.noreply.github.com>
2021-08-06 10:11:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "🔗 JOIN OUR GROUP 🔗", url="https://t.me/MovieCupboard")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                            "🎥 Movie Cupboard 🎥", url="https://t.me/MovieCupboard")
                     ]
                 ]
             ),
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")
+                            "Contact Me", url="https://t.me/owner_moviecupboard")
                     ]
                 ]
             ),
